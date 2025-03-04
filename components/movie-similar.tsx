eedbd2ee9b82fda6f4aa0702c591df5f8a217c81
homeCom <rhj1303@naver.com>
2025-03-04 21:49:52 +0900

    [2025.03.04] similar 컴포넌트 재수정

--- components/movie-similar.tsx
+++ components/movie-similar.tsx
@@ -5,6 +5,7 @@ import styles from '../styles/movie-similar.module.css'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Link from "next/link";
 
 export async function getSimilarMovie(id : string){
     const response = await fetch(`${API_URL}/${id}/similar`)
@@ -23,19 +24,19 @@ export default async function MovieSimilar({id} : {id:string}){
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 5,
+        slidesToShow: 6,
         slidesToScroll: 1,
       };
 
     return ( 
-        <div style={{margin:"100px"}}>
-            <h2 style={{textAlign:"center", fontWeight:"bold", fontSize:"25px"}}>Similar Movies</h2>
+        <div style={{margin:"50px"}}>
+            <h2 className={styles.title}>Recommend Movies</h2>
             <Slider {...settings}>
             {movies.map((movie, index) => (
-                <div key={index} className={styles.contentBox}>
-                    <img style={{height:"200px"}} src={movie.poster_path} alt={movie.title} />
+                <Link href={`/movies/${movie.id}`} key={index} className={styles.contentBox}>
+                    <img className={styles.poster} src={movie.poster_path} alt={movie.title} />
                     <span>{movie.title}</span>
-                </div>
+                </Link>
             ))}
         </Slider>
         </div>
